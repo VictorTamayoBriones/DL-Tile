29dee222f8aa78b91b8f2059d42cab1f1f5ea7e3
raul tamayo briones <51513985+raul051@users.noreply.github.com>
2020-09-24 23:18:54 -0500

    Cambios visuales y rutas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-    
-    var despliegue = document.querySelector('#servicies');
-=======
-
     var btnDespliegue = document.querySelector('#servicies');
->>>>>>> 636c50b2a62177d4bd80126e1668b573aeb48d3c
     var desplegable = document.querySelector('.desplegable');
     
     var btnMenu = document.querySelector('#menu');
